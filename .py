d8cb2d22c64b9d35acdcbec175923a4615fb4b2b
Enoch Numo Tetteh <154479277+numoenochtetteh@users.noreply.github.com>
2025-01-20 18:49:10 +0000

    Update .py

--- .py
+++ .py
@@ -1,3 +1,13 @@
 # to start the project you need to install and configure AWS CLI (AMAZON WEB SERVICES COMMAND LINE INTERFACE)
-# after installing run AWS CLI run this command to  confirm 
-aws -- version 
+# To confirm the installation, use the 
+aws --version #command at a command prompt 
+
+#REASON FOR AWS Command Line Interface
+#The AWS CLI allows you to interact with AWS services (like DynamoDB, S3, or EC2) directly from your terminal
+#Configuring the CLI links your local machine to your AWS account. It ensures that your credentials (like Access Key ID and Secret Access Key) are properly set up to authenticate your action
+
+#After that create your access key and secret key in IAM to help you interact with AWS in AWS CLI
+#Open a terminal on your local machine and enter
+aws configure. #Enter the access key and secret access key and  Choose a default region name
+
+
