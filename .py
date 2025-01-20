27e608edea98c23e3eb0a8161ee71dbf47a27f59
Enoch Numo Tetteh <154479277+numoenochtetteh@users.noreply.github.com>
2025-01-20 18:24:41 +0000

    codezon .py

---
+++ .py
@@ -0,0 +1,3 @@
+# to start the project you need to install and configure AWS CLI (AMAZON WEB SERVICES COMMAND LINE INTERFACE)
+# after installing run AWS CLI run this command to  confirm 
+aws -- version 
